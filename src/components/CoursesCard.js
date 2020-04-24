704a967c52d41b6a581d63015715fa52375912af
pistachio05 <malaya@uci.edu>
2020-04-23 23:24:13 -0700

    init

--- src/components/CoursesCard.js
+++ src/components/CoursesCard.js
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import Card from './Card';
+
 import API_URLS from '../config/config';
 
 function CoursesCard() {
     const [courseData, setCourseData] = useState([]);
 
     useEffect(() => {
-      console.log("END-POINTS", API_URLS);
-      async function fetchCourses() {
-        console.log('fetching getCourses', API_URLS.getCourses);
-        const res = await fetch(API_URLS.getCourses);
-
+      const fetchCourses = async() => {
+          console.log('fetching getCourses', API_URLS.getCourses);
+          const res = await fetch(API_URLS.getCourses);
+          const coursesArray = await res.json();
+          setCourseData(coursesArray);
       }
+      fetchCourses();
     }, []);
     return (
       <React.Fragment>
-       <div>
-           COURSES CARD COMING SOON
-       </div>
+      {courseData.map(item => (
+         <Card key={item.name} name={item.name} id={item.link} />
+      ))}
       </React.Fragment>
     );
   }
--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const NODE_ENV = process.env.NODE_ENV;
 console.log("NODE_ENV:", NODE_ENV);  
 
 const prodUrl = 'aws';
-const devUrl = 'localhost:8000'
+const devUrl = 'http://localhost:8000'
 
 const baseUrl = NODE_ENV === 'production' ? prodUrl : devUrl;
 
